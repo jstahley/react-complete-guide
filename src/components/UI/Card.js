8784f2aafa9acf98f9783ce1eb59e05677bdcf6a
Jason <devguy@jaystahley.me>
2021-09-24 17:08:47 -0400

    Css/JS refactor

---
+++ src/components/UI/Card.js
@@ -0,0 +1,10 @@
+import '../UI/Card.css'
+
+function Card (props) {
+    const className = 'card ' + props.className;
+return <div className={className}>
+{props.children}
+</div>
+}
+
+export default Card;
